28e4fb25928eff6b02b220be1dbc228115c5ed61
Shubham Akyamwar <akyamwars@gmail.com>
2025-07-31 20:21:22 +0530

    Add files via upload

---
+++ C -modules/assignment 1/odd even.c
@@ -0,0 +1,14 @@
+//Write a program to check whether a number is even or odd.
+#include<stdio.h>
+void main()
+{
+	int a=10;
+	if(a%2==0){
+	
+	printf("no is even ");
+	}
+
+    else{
+    	printf("no is odd ");
+	}
+	}
