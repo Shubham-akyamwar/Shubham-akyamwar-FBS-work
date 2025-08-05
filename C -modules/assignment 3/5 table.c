f4463f203e43ab8c11327786843abeafcb3b6395
Shubham Akyamwar <akyamwars@gmail.com>
2025-08-06 02:02:08 +0530

    Add files via upload

---
+++ C -modules/assignment 3/5 table.c
@@ -0,0 +1,14 @@
+//Print table for given number.
+//Input: n = 5
+//Output: 5 10 15 20 25 30 35 40 45 50
+#include<stdio.h>
+void main(){
+	int i=0,a=5,table;
+	for(a=5;i<=10;i++)
+	{
+		table=a*i;
+		printf("%d\n",table);
+	}
+	
+	
+}
