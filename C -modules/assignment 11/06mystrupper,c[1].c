cdd234df9390518177cd3a8707eedda4aadadc5d
Shubham Akyamwar <akyamwars@gmail.com>
2025-09-02 23:36:33 +0530

    Add files via upload

---
+++ C -modules/assignment 11/06mystrupper,c[1].c
@@ -0,0 +1,18 @@
+//mystrupper user define function
+#include<stdio.h>
+#include<string.h>
+void mystrupper(char*);
+
+void main()
+{
+	char str[]="shubham";
+	mystrupper(str);
+}
+void mystrupper(char* str)
+{
+	int i;
+	for( i=0;str[i]!='\0';i++){
+		str[i]=str[i]-32;
+	}
+	puts(str);
+}
---
+++ C -modules/assignment 11/09mystrstr[1].c
@@ -0,0 +1,15 @@
+//mystrstr user define function
+#include<stdio.h>
+#include<string.h>
+int mystrstr(char*,char*);
+void main()
+{
+	char str[]="hii shubham akyamwar";
+	char sub[]="shubham";
+	 mystrstr(str,sub);
+
+}
+int mystrstr(char* str,char* sub)
+{
+	while(str==sub)
+}
